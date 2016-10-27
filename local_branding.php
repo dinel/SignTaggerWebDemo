233e666793fdec71f854c5639fd1d0c402c2d459
Constantin Orasan <c.orasan@wlv.ac.uk>
2016-10-27 12:18:56 +0100

    Local customisation

---
+++ local_branding.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * Copyright 2016 dinel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+?>
+
+<!-- 
+    Include anything that you would like to be specific for your site.
+    It will appear in the <div> after the links to GitHub. Ideally it 
+    should be quite brief like a link to your homepage.
+-->
+
+<div><a href="http://rgcl.wlv.ac.uk">Back to RGCL</a></div>
